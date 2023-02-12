4ce6f7e41232977db0441f418253e0629493155f
Dejan Dmitrović <dmitrovyu@users.noreply.github.com>
2023-02-12 21:05:24 +0100

    Update BandMatrix.java

--- Week2/BandMatrix.java
+++ Week2/BandMatrix.java
@@ -1,3 +1,13 @@
+/**************
+* BandMatrix.java takes two integer command-line arguments n and width and prints
+* an n-by-n pattern, with a zero (0) for each element whose distance from the main diagonal
+* is strictly more than width, and an asterisk (*) for each entry that is not, and two spaces between each 0 or *.
+* https://coursera.cs.princeton.edu/introcs/assignments/loops/specification.php
+* @author Dejan Dmitrovic
+****************/
+
+
+
 public class BandMatrix {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
