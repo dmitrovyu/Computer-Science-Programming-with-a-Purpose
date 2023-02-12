9f873a71c718cc812e0658074247b2a0629885cc
Dejan Dmitrović <dmitrovyu@users.noreply.github.com>
2023-02-12 21:09:20 +0100

    Update GeneralizedHarmonic.java

--- Week2/GeneralizedHarmonic.java
+++ Week2/GeneralizedHarmonic.java
@@ -1,3 +1,10 @@
+/***********************
+* Takes two integer command-line arguments n and r and uses
+* a for loop to compute the nth generalized harmonic number of order r
+* @author Dejan Dmitrovic
+* https://coursera.cs.princeton.edu/introcs/assignments/loops/specification.php
+************************/
+
 public class GeneralizedHarmonic {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
