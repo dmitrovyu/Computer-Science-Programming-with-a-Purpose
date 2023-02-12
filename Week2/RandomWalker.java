b1f21d60d4f379900b8ee54edea22b46c557e720
Dejan Dmitrović <dmitrovyu@users.noreply.github.com>
2023-02-12 21:14:06 +0100

    Update RandomWalker.java

--- Week2/RandomWalker.java
+++ Week2/RandomWalker.java
@@ -1,3 +1,13 @@
+/*********************************
+* A Java programmer begins walking aimlessly. At each time step, 
+* she takes one step in a random direction (either north, east, south, or west), 
+* each with probability 25%. She stops once she is at Manhattan distance r from 
+* the starting point. How many steps will the random walker take?
+*
+* @author Dejan Dmitrovic
+* https://coursera.cs.princeton.edu/introcs/assignments/loops/specification.php
+**********************************/
+
 public class RandomWalker {
     public static void main(String[] args) {
         int r = Integer.parseInt(args[0]);
